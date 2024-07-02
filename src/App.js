4c468ea903e4c9c69b22e6ec196d6e4f9422d0d3
Disha453 <dishasanghani.dreamspot@gmail.com>
2024-07-02 16:35:25 -0700

    change

--- src/App.js
+++ src/App.js
@@ -42,39 +42,40 @@ function App() {
 
   return (
     <>
-      <table>
-        <thead>
-          <th>
-            <label htmlFor="fav.lan."><b>Fav_Language</b></label>
-            <input type="checkbox" name="lang" checked={langg.length === 3 ? true : false} onChange={(e) => checkChange(e)} />
-          </th>
-        </thead>
+      <div style={{ display: "flex", justifyContent: "center", marginTop: "10%" }}>
+        <table>
+          <thead>
+            <th>
+              <label htmlFor="fav.lan."><b>Fav_Language</b></label>
+              <input type="checkbox" name="lang" checked={langg.length === 3 ? true : false} onChange={(e) => checkChange(e)} />
+            </th>
+          </thead>
 
 
-        <tbody>
-          <tr style={{ display: "flex", flexDirection: "column" }}>
+          <tbody>
+            <tr style={{ display: "flex", flexDirection: "column" }}>
 
-            <td style={{ display: "flex" }}>
-              <label htmlFor="fav.lan.">javascript</label>
-              <input type="checkbox" name="fav.lan." id="javascript" value="javascript" checked={langg.includes("javascript")} onChange={(e) => checkSubmit(e)} />
-            </td>
+              <td style={{ display: "flex" }}>
+                <label htmlFor="fav.lan.">javascript</label>
+                <input type="checkbox" name="fav.lan." id="javascript" value="javascript" checked={langg.includes("javascript")} onChange={(e) => checkSubmit(e)} />
+              </td>
 
-            <td style={{ display: "flex" }}>
-              <label htmlFor="fav.lan.">html</label>
-              <input type="checkbox" name="fav.lan." id="html" value="html" checked={langg.includes("html")} onChange={(e) => checkSubmit(e)} />
-            </td>
+              <td style={{ display: "flex" }}>
+                <label htmlFor="fav.lan.">html</label>
+                <input type="checkbox" name="fav.lan." id="html" value="html" checked={langg.includes("html")} onChange={(e) => checkSubmit(e)} />
+              </td>
 
-            <td style={{ display: "flex" }}>
-              <label htmlFor="fav.lan.">css</label>
-              <input type="checkbox" name="fav.lan." id="css" value="css" checked={langg.includes("css")} onChange={(e) => checkSubmit(e)} />
-            </td>
-          </tr>
-        </tbody>
-      </table>
-
-
+              <td style={{ display: "flex" }}>
+                <label htmlFor="fav.lan.">css</label>
+                <input type="checkbox" name="fav.lan." id="css" value="css" checked={langg.includes("css")} onChange={(e) => checkSubmit(e)} />
+              </td>
+            </tr>
+          </tbody>
+        </table>
 
 
+
+      </div>
     </>
   );
 }
